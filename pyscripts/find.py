559b7ca45583a17567c4e3f0a85473affd83098b
yangmin.zhao <yangminz@users.noreply.github.com>
2022-07-07 18:39:28 +0800

    use new key title

--- pyscripts/find.py
+++ pyscripts/find.py
@@ -1,17 +1,17 @@
 import re
 
 def find_value_by_key_from_text_lines(filename, searchkey):
-    # ~~ key
+    # KEY:: key
     # value
     result = []
     with open(filename, "r", encoding="utf-8") as fr:
         content = fr.read()
-        key_matches = re.finditer(r"(?<=\n\~\~)\s*(?P<key1>[^\n]+)(?=\n)|^\~\~\s*(?P<key2>[^\n]+)(?=\n)", content)
+        key_matches = re.finditer(r"(?<=\nKEY::)\s*(?P<key1>[^\n]+)(?=\n)|^KEY::\s*(?P<key2>[^\n]+)(?=\n)", content)
         for match in key_matches:
             key = "".join([x if x != None else "" for x in [match.group("key1"), match.group("key2")]])
             if searchkey.lower() in key.lower():
                 valStartIndex = match.span()[1]
-                valueSpan = re.search(r".*(?=\~\~)", content[valStartIndex + 1:]).span()
+                valueSpan = re.search(r".*(?=KEY::)", content[valStartIndex + 1:]).span()
                 value = content[valStartIndex + 1 : valueSpan[1] + valStartIndex + 1].rstrip("\n")
                 result += [(key, value)]
     return result
